a507f45e9a650292d958af513a10d441b94c250e
Zahkc <zahkcb@gmail.com>
2022-10-06 13:17:50 +1100

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,21 +6,69 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <video className="App-camera" id="local_cam" autoPlay></video>
+      <video className="App-video" id="local_vid" autoPlay></video>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Video Test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <table>
+          <tr>
+            <td>
+            <button onClick={getMedia}>Get Media</button>
+            </td>
+            <td>
+            <button onClick={swapScreen}>Swap Screen</button>
+            </td>
+            <td>
+            <button onClick={stopStream}>Stop Screen</button>
+            </td>
+          </tr>
+        </table>
+
+
       </header>
     </div>
   );
+}
+let camStream = null;
+let vidStream = null;
+let localCam = null;
+let localVid = null;
+
+
+async function getMedia(){
+  localCam = document.getElementById('local_cam');
+  localVid = document.getElementById('local_vid');
+  camStream = await navigator.mediaDevices.getUserMedia({
+         video: { mediaSource: "camera" },
+         audio: true
+       });
+  vidStream = await navigator.mediaDevices.getDisplayMedia({
+         video: { mediaSource: "screen" },
+         audio: false
+       });
+  startStream()
+}
+
+async function startStream(){
+  localCam.srcObject = camStream
+  localVid.srcObject = vidStream
+}
+
+async function swapScreen(){
+  vidStream = await navigator.mediaDevices.getDisplayMedia({
+         video: { mediaSource: "screen" },
+         audio: false
+       });
+  startStream()
+}
+
+async function stopStream(){
+  vidStream = null
+  camStream = null
+  localCam.srcObject = null
+  localVid.srcObject = null
 }
 
 export default App;
