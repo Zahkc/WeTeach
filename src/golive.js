832aadd660726ddab0c4328b0e28dc6709d9c668
Tom Hobeika <65552757+tomhobeika@users.noreply.github.com>
2022-12-12 15:31:05 +1100

    Broken chat

--- src/golive.js
+++ src/golive.js
@@ -17,6 +17,9 @@ let outStream = new MediaStream([]);
 let localCam = null;
 let localVid = null;
 var myusername = Janus.randomString(12);
+var participants = {};
+var transactions = {};
+var textroom = null;
 
 let stage = 0;
 
@@ -48,13 +51,13 @@ function GoLive() {
           <video className="App-camera" id="local_cam" autoPlay></video>
           </tr>
           <tr>
-          <textarea disabled className="chat_window" ></textarea>
+          <textarea disabled className="chat_window" id = "chat" ></textarea>
           </tr>
           <tr>
           <td>
-          <textarea className="new_message"></textarea>
+          <textarea className="new_message" id = "msg_box"></textarea>
           </td>
-          <button>Send</button>
+          <button onClick={sendData}>Send</button>
           <td>
           </td>
           </tr>
@@ -202,13 +205,11 @@ async function toggleScreenShare(){
 
 
 function initJanus(){
-
     Janus.init({debug: "all", callback: function() {
         if(!Janus.isWebrtcSupported()) {
     console.log("No WebRTC support... ");
     return;
         }
-
         janus = new Janus(
     {
 			server: server,
@@ -230,6 +231,7 @@ function initJanus(){
           opaqueId: opaqueId,
           success: function(pluginHandle) {
               screentest = pluginHandle;
+              textroom = pluginHandle;
               Janus.log("Plugin attached! (" + screentest.getPlugin() + ", id=" + screentest.getId() + ")");
               // Prepare the username registration
           },
@@ -293,7 +295,41 @@ function initJanus(){
 								Janus.debug("Handling SDP as well...", jsep);
 								screentest.handleRemoteJsep({ jsep: jsep });
 							}
+          },
+          ondataopen: function(data) {
+            Janus.log("The DataChannel is available!");
+         
+          },
+          ondata: function(data) {
+            Janus.debug("We got data from the DataChannel!", data);
+            //~ $('#datarecv').val(data);
+            var json = JSON.parse(data);
+            var transaction = json["transaction"];
+            if(transactions[transaction]) {
+              // Someone was waiting for this
+              transactions[transaction](json);
+              delete transactions[transaction];
+              return;
             }
+            var what = json["textroom"];
+            if(what === "message") {
+              // Incoming message: public or private?
+              var msg = escapeXmlTags(json["text"]);
+              var from = json["from"];
+              var dateString = getDateString(json["date"]);
+              var whisper = json["whisper"];
+              var sender = participants[from] ? participants[from] : escapeXmlTags(json["display"]);
+                // Public message
+                var chat = document.getElementById("chat").value;
+                Janus.log('<p>[' + dateString + '] <b>' + sender + ':</b> ' + msg);
+                //$('#chatroom').get(0).scrollTop = $('#chatroom').get(0).scrollHeight;
+              }
+             
+          },
+          oncleanup: function() {
+            Janus.log(" ::: Got a cleanup notification :::");
+            //$('#datasend').attr('disabled', true);
+          }
         });
 
       },
@@ -305,9 +341,9 @@ function initJanus(){
                     setJanusInstance(null);
       }
             });
+            
     }});
 }
-
 
 
 function shareScreen() {
@@ -340,6 +376,58 @@ function shareScreen() {
 		}
 	}});
 }
+function escapeXmlTags(value) {
+	if(value) {
+		var escapedValue = value.replace(new RegExp('<', 'g'), '&lt');
+		escapedValue = escapedValue.replace(new RegExp('>', 'g'), '&gt');
+		return escapedValue;
+	}
+}
+// Helper to format times
+function getDateString(jsonDate) {
+	var when = new Date();
+	if(jsonDate) {
+		when = new Date(Date.parse(jsonDate));
+	}
+	var dateString =
+			("0" + when.getUTCHours()).slice(-2) + ":" +
+			("0" + when.getUTCMinutes()).slice(-2) + ":" +
+			("0" + when.getUTCSeconds()).slice(-2);
+	return dateString;
+}
+// Just an helper to generate random usernames
+function randomString(len, charSet) {
+  charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var randomString = '';
+  for (var i = 0; i < len; i++) {
+    var randomPoz = Math.floor(Math.random() * charSet.length);
+    randomString += charSet.substring(randomPoz,randomPoz+1);
+  }
+  return randomString;
+}
 
-
+// Sends chat message
+function sendData() {
+	var messageText = document.getElementById("msg_box").value;
+	if(messageText === "") {
+    // Does nothing
+		return;
+	}
+	var message = {
+		textroom: "message",
+		transaction: randomString(12),
+		room: room,
+ 		text: messageText
+	};
+	// Note: messages are always acknowledged by default. This means that you'll
+	// always receive a confirmation back that the message has been received by the
+	// server and forwarded to the recipients. If you do not want this to happen,
+	// just add an ack:false property to the message above, and server won't send
+	// you a response (meaning you just have to hope it succeeded).
+	textroom.data({
+		text: JSON.stringify(message),
+		error: function(reason) { Janus.log(reason); },
+		success: function(message) { Janus.log(messageText); }
+	});
+}
 export default GoLive;
