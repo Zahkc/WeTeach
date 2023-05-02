3cce46261f2fe54baf25e87e41d2e8a530a64bae
dp964 <dp964@users.noreply.github.com>
2023-05-02 12:03:53 +0000

    Fix settings to point to local server

--- src/components/janus/settings.js
+++ src/components/janus/settings.js
@@ -3,9 +3,9 @@
 // Server running janus for routing traffic to.
 export const server = "https://weteach.ddns.net:8089/janus";
 
-export const dbdaemon = "https://weteach.ddns.net:5000";
+export const dbdaemon = "https://localhost:5000";
 
-export const uploaddaemon = "https://weteach.ddns.net:5002";
+export const uploaddaemon = "https://localhost:5002";
 
 // For future load scaling
 var iceServers = null;
