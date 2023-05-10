ccdcaef44591419cc03d54f7d357dd535247c1a8
dp964 <dpritchard96@gmail.com>
2023-05-10 10:07:09 +1000

    Update Settings
    Dev vs production build use different API endpoints

--- src/components/janus/settings.js
+++ src/components/janus/settings.js
@@ -7,15 +7,8 @@ export const server = "https://weteach.ddns.net:8089/janus";
 // Change when dev testing API on localhost
 export const mediaserver = "https://weteach.ddns.net/";
 
-
-// Dev
-export const dbdaemon = "http://localhost:5000";
-
-export const uploaddaemon = "http://localhost:5002";
-
-// Build
-//export const dbdaemon = "https://weteach.ddns.net/";
-//export const uploaddaemon = "https://weteach.ddns.net/ds/";
+export const dbdaemon = process.env.NODE_ENV === "production" ? "https://weteach.ddns.net/" : "http://localhost:5000";
+export const uploaddaemon = process.env.NODE_ENV === "production" ? "https://weteach.ddns.net/ds/" : "http://localhost:5002";
 
 
 // For future load scaling
